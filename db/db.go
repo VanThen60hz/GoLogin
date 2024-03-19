eb240c093086a18f8ea316abda6744b98149154a
VanThen60hz <nguyenvthang2409@gmail.com>
2024-03-20 03:01:07 +0700

    fix: sqlite3 not working

--- db/db.go
+++ db/db.go
@@ -2,38 +2,41 @@ package db
 
 import (
 	"database/sql"
+	"fmt"
 
-	_ "github.com/mattn/go-sqlite3"
+	_ "modernc.org/sqlite"
 )
 
 var DB *sql.DB
 
 func InitDB() {
-	DB, err := sql.Open("sqlite3", "api.db")
+	db, err := sql.Open("sqlite", "api.sql")
 	if err != nil {
-		panic("Could not connect to database.")
+		panic("Database could not connect: " + err.Error())
 	}
 
-	DB.SetMaxOpenConns(10)
-	DB.SetMaxIdleConns(5)
+	DB = db
 
-	createTable()
+	err = createTables()
+	if err != nil {
+		panic("Database could not connect: " + err.Error())
+	}
+
+	fmt.Println("Tables created successfully!")
 }
 
-func createTable() {
+func createTables() error {
 	createEventsTable := `
-		CREATE TABLE IF NOT EXISTS events (
-			id INTEGER PRIMARY KEY AUTOINCREMENT,
-			name TEXT NOT NULL,
-			description TEXT NOT NULL,
-			location TEXT NOT NULL,
-			dateTime DATETIME NOT NULL,
-			user_id INTEGER
-		)
-	`
+        CREATE TABLE IF NOT EXISTS events (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT NOT NULL,
+            description TEXT NOT NULL,
+            location TEXT NOT NULL,
+            dateTime DATETIME NOT NULL,
+            user_id INTEGER
+        )
+    `
 
 	_, err := DB.Exec(createEventsTable)
-	if err != nil {
-		panic("Could not create events table.")
-	}
+	return err
 }
--- main.go
+++ main.go
@@ -13,14 +13,18 @@ func main() {
 	db.InitDB()
 	server := gin.Default()
 
-	server.GET("/events", getEvents)
+	server.GET("/events", getEvents) // GET, POST, PUT, PATCH, DELETE
 	server.POST("/events", createEvent)
 
 	server.Run(":8080") // localhost:8080
 }
 
 func getEvents(context *gin.Context) {
-	events := models.GetAllEvents()
+	events, err := models.GetAllEvents()
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not fetch events. Try again later."})
+		return
+	}
 	context.JSON(http.StatusOK, events)
 }
 
@@ -28,19 +32,18 @@ func createEvent(context *gin.Context) {
 	var event models.Event
 	err := context.ShouldBindJSON(&event)
 	if err != nil {
-		context.JSON(http.StatusBadRequest, gin.H{
-			"message": "Could not parse request data",
-		})
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse request data."})
 		return
 	}
 
 	event.ID = 1
 	event.UserID = 1
 
-	event.Save()
+	err = event.Save()
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not create event. Try again later."})
+		return
+	}
 
-	context.JSON(http.StatusCreated, gin.H{
-		"message": "Event created!",
-		"event":   event,
-	})
+	context.JSON(http.StatusCreated, gin.H{"message": "Event created!", "event": event})
 }
