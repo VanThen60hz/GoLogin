7ed20b7833b79f24aa0266c8cc4b1b7781e1f08d
VanThen60hz <nguyenvthang2409@gmail.com>
2024-04-11 17:44:57 +0700

    feature: token verification

--- routes/events.go
+++ routes/events.go
@@ -35,6 +35,15 @@ func getEventById(context *gin.Context) {
 }
 
 func createEvent(context *gin.Context) {
+	token := context.Request.Header.Get("Authorization")
+
+	if token == "" {
+		context.JSON(http.StatusUnauthorized, gin.H{
+			"message": "Not authorized",
+		})
+		return
+	}
+
 	var event models.Event
 	err := context.ShouldBindJSON(&event)
 	if err != nil {
