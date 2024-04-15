b8475caa04c20ba8198d0423b429219430f2110e
VanThen60hz <nguyenvthang2409@gmail.com>
2024-04-16 02:26:53 +0700

    feature: registering users

--- db/db.go
+++ db/db.go
@@ -2,6 +2,7 @@ package db
 
 import (
 	"database/sql"
+	"fmt"
 
 	_ "modernc.org/sqlite"
 )
@@ -53,17 +54,18 @@ func createTables() {
 	}
 
 	createRegistrationsTable := `
-		CREATE TABLE IF NOT EXIST registrations (
+		CREATE TABLE IF NOT EXISTS registrations (
 			id INTEGER PRIMARY KEY AUTOINCREMENT,
-			event_id INTEGER
-			user_id	INTEGER
-			FOREIGN KEY (event_id) REFERENCES events(id)
+			event_id INTEGER,
+			user_id	INTEGER,
+			FOREIGN KEY (event_id) REFERENCES events(id),
 			FOREIGN KEY (user_id) REFERENCES users(id)
 		)
 	`
 
 	_, err = DB.Exec(createRegistrationsTable)
 	if err != nil {
+		fmt.Print(err.Error())
 		panic("Could not create registrations table.")
 	}
 }
