1cd811a0de80a27266137719c628ef517fcb0159
VanThen60hz <nguyenvthang2409@gmail.com>
2024-04-16 02:48:45 +0700

    feature: cancel registration

--- routes/register.go
+++ routes/register.go
@@ -38,4 +38,25 @@ func registerForEvent(context *gin.Context) {
 }
 
 func cancelRegistration(context *gin.Context) {
+	userId := context.GetInt64("userId")
+	eventId, err := strconv.ParseInt(context.Param("id"), 10, 64)
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse event ID. Try again later."})
+		return
+	}
+
+	var event models.Event
+	event.ID = eventId
+
+	err = event.CancelRegistration(userId)
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{
+			"message": "Could not cancel registration for event",
+		})
+		return
+	}
+
+	context.JSON(http.StatusCreated, gin.H{
+		"message": "Cancelled!",
+	})
 }
